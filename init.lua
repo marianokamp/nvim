a1ff4742f0b78a398ecb3924e0ab305b13e94abc
Mariano Kamp <mariano.kamp@gmail.com>
2024-04-01 15:59:46 +0200

    [added] pyright and black.

--- init.lua
+++ init.lua
@@ -4,4 +4,5 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.g.lazyvim_python_lsp = "basedpyright"
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
