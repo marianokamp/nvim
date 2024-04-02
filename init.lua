5ce4df944bf2ed79cc353d4a45ee8ebbddebc207
Mariano Kamp <mariano.kamp@gmail.com>
2024-04-02 16:47:45 +0200

    [changed] diagnostics windows are now floating with a border around them.

--- init.lua
+++ init.lua
@@ -4,5 +4,12 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Turn the diagnostics into floating window with borders
+vim.diagnostic.config({
+  virtual_text = false,
+  float = { border = "rounded" },
+})
+
 vim.g.lazyvim_python_lsp = "basedpyright"
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+-- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
